de2519866bd581415adee25519a50b2b72a24ee8
kokiyoshida <lllttt06@gmail.com>
2023-03-02 09:23:09 +0900

    Film.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,52 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+    @8191
+    D=A
+    @number_of_pixels
+    M=D
+
+(INF_LOOP)
+    // ループの回数(0~number_of_pixelまでループする)
+    @loop_count  
+    M=0
+    // keyboardの入力を監視する
+    @KBD
+    D=M
+    @BLACK_SCREEN
+    D; JGT
+
+// スクリーンを黒くする
+(BLACK_SCREEN)
+    @loop_count
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1
+    @loop_count
+    MD=M+1
+    @number_of_pixels
+    D=D-M
+    @BLACK_SCREEN
+    D; JLT
+    @INF_LOOP
+    D; JGE
+
+// スクリーンを白くする
+(WHITE_SCREEN)
+    @loop_count
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+    @loop_count
+    MD=M+1
+    @number_of_pixels
+    D=D-M
+    @WHITE_SCREEN
+    D; JLT
+
+// INF_LOOP
+    @INF_LOOP
+    0; JMP
